7f16814db6319a7ccaba4819f8932a93b79bd08e
Bianshilong <442668830@qq.com>
2017-12-22 14:09:44 +0800

    add for product description

---
+++ IStore/src/com/bsl/entity/Product.java
@@ -0,0 +1,19 @@
+package com.bsl.entity;
+
+public class Product {
+	/**
+	 * product
+	 * pId   			商品ID			Long
+	 * pName 	    	商品名称			String
+	 * pPrice			商城价			Double
+	 * 
+	 * pMarketPrice		市场价			Double
+	 * pImg				商品图片			String
+	 * pDesc			商品描述			String
+	 * 
+	 * pTime			上架时间			String
+	 * pHot				是否热门			Boolean
+	 * pFlag			是否下架			Boolean
+	 *c_id				分类id			Category
+	 */
+}
